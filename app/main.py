6a0844425aef947637c98a036f53e0806eea2b07
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-06-08 14:53:14 +0300

    Update solution

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import multiprocessing
 import time
+from concurrent.futures import ProcessPoolExecutor, wait
 from hashlib import sha256
 
 
-PASSWORDS_TO_BRUTE_FORCE = [
+PASSWORDS_TO_BRUTE_FORCE = {
     "b4061a4bcfe1a2cbf78286f3fab2fb578266d1bd16c414c650c5ac04dfc696e1",
     "cf0b0cfc90d8b4be14e00114827494ed5522e9aa1c7e6960515b58626cad0b44",
     "e34efeb4b9538a949655b788dcb517f4a82e997e9e95271ecd392ac073fe216d",
@@ -14,53 +15,41 @@ PASSWORDS_TO_BRUTE_FORCE = [
     "1273682fa19625ccedbe2de2817ba54dbb7894b7cefb08578826efad492f51c9",
     "7e8f0ada0a03cbee48a0883d549967647b3fca6efeb0a149242f19e4b68d53d6",
     "e5f3ff26aa8075ce7513552a9af1882b4fbc2a47a3525000f6eb887ab9622207",
-]
+}
 
 
 def sha256_hash_str(to_hash: str) -> str:
     return sha256(to_hash.encode("utf-8")).hexdigest()
 
 
-def worker(
-        start: int, end: int, shared_list: list, lock: bool, stop_flag
-) -> None:
+def worker(start: int, end: int) -> list:
+    found = []
     for i in range(start, end):
-        if stop_flag.value:
-            return
-
         if sha256_hash_str(f"{i:08}") in PASSWORDS_TO_BRUTE_FORCE:
-            with lock:
-                print(f"Process {multiprocessing.current_process().name} found {i:08}")
-                shared_list.append(f"{i:08}")
-                if len(shared_list) >= 10:
-                    stop_flag.value = True
-                    return
+            print(f"Process {multiprocessing.current_process().name} found {i:08}")
+            found.append(f"{i:08}")
+
+    return found
 
 
 def brute_force_password() -> None:
-    manager = multiprocessing.Manager()
-    shared_list = manager.list()
-    lock = manager.Lock()
-    stop_flag = manager.Value("b", False)
-
     total_range = 99999999
     num_processes = multiprocessing.cpu_count() - 1
-    print(f"Number of processes: {num_processes}")
     chunk_size = total_range // num_processes
-    print(f"Number of iterations for each process: {chunk_size}")
 
     processes = []
-    for i in range(num_processes):
-        start = i * chunk_size + 1
-        end = (i + 1) * chunk_size + 1
-        process = multiprocessing.Process(
-            target=worker, args=(start, end, shared_list, lock, stop_flag)
-        )
-        process.start()
-        processes.append(process)
 
+    with ProcessPoolExecutor(multiprocessing.cpu_count()) as executor:
+        for i in range(num_processes):
+            start = i * chunk_size + 1
+            end = (i + 1) * chunk_size + 1
+            processes.append((executor.submit(worker, start, end)))
+
+    wait(processes)
+
+    found_passwords = []
     for process in processes:
-        process.join()
+        found_passwords.extend((process.result()))
 
 
 if __name__ == "__main__":
